28b5cd90dfe34b44b3fd3c4592ca20b655629fc6
Clésio Jr <clesiorsilvajunior@gmail.com>
2025-02-11 16:50:53 -0300

    Ajustes no backend e frontend funcionando

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,22 @@ from bs4 import BeautifulSoup
 #aaaa
 
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = [
+ 'http://localhost:3000'
+]
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=origins,
+ allow_credentials=True,
+ allow_methods=['*'],
+ allow_headers=['*']
+)
+
 
 @app.get("/")
 def read_root():
