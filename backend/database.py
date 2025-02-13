17b99121b7840bf7ef59ce89f3b013d3c8748064
Clésio Jr <clesiorsilvajunior@gmail.com>
2025-02-13 18:44:12 -0300

    Testando alteracao dos valores de acesso

--- backend/database.py
+++ backend/database.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-user = os.getenv("user", "default_user")
-password = os.getenv("password", "default_password")
-host = os.getenv("host", "localhost")
-database = os.getenv("database", "default_db")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+database = os.getenv("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
